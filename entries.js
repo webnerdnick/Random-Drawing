47806eabf55427b6d3d36d5ca534302ba082c974
webnerdnick <webnerdnick@gmail.com>
2019-09-03 12:59:20 -0400

    Added entries into external file

---
+++ entries.js
@@ -0,0 +1,14 @@
+var entries = {
+    'A': {
+        'entries': 1000,
+        'color': getRandomColor()
+    },
+    'B': {
+        'entries': 2000,
+        'color': getRandomColor()
+    },
+    'C': {
+        'entries':1500,
+        'color': getRandomColor()
+    }
+}
