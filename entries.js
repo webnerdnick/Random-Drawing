914a5ecdecdfdafd2091998518b10e9a805391ba
McBride <njm5850@psu.edu>
2019-10-01 09:45:24 -0400

    Wednesday Night Pick

--- entries.js
+++ entries.js
@@ -1,14 +1,46 @@
 var entries = {
-    'A': {
-        'entries': 5000,
+    'Ryan': {
+        'entries': 1600,
         'color': getRandomColor()
     },
-    'B': {
-        'entries': 4200,
+    'Jonathan': {
+        'entries': 2550,
         'color': getRandomColor()
     },
-    'C': {
-        'entries':600,
+    'Lilian': {
+        'entries':1700,
+        'color': getRandomColor()
+    },
+    'Lauren': {
+        'entries':800,
+        'color': getRandomColor()
+    },
+    'Hunter': {
+        'entries':2150,
+        'color': getRandomColor()
+    },
+    'Josh': {
+        'entries':2000,
+        'color': getRandomColor()
+    },
+    'Matt': {
+        'entries':1500,
+        'color': getRandomColor()
+    },
+    'Chandler': {
+        'entries':1100,
+        'color': getRandomColor()
+    },
+    'Tanner': {
+        'entries':1200,
+        'color': getRandomColor()
+    },
+    'Daisy': {
+        'entries':2600,
+        'color': getRandomColor()
+    },
+    'Kaylynn': {
+        'entries':200,
         'color': getRandomColor()
     }
 }
